ef047f1a6892e22b6f10006c741c449cdd53125c
CarmelonHaldon <carmelonhaldon@gmail.com>
2019-06-04 12:03:29 +0200

    Primera versión...

---
+++ win/bat/sts/SpringToolSuite4.bat
@@ -0,0 +1,4 @@
+cd "C:\__home\CarmelonHaldon\bin\spring-tool-suite-4-4.1.2.RELEASE-e4.10.0-win32.win32.x86_64"
+rem TODO E03836: Por Git...
+set HOME=C:\__home\CarmelonHaldon\user
+start "" SpringToolSuite4.exe
